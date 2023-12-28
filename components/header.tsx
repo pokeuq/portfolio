0671bddb79fa1c0cc32504c440947710ca09f277
pokeuq <109028724+pokeuq@users.noreply.github.com>
2023-12-28 14:31:06 +0500

    fixed active section

--- components/header.tsx
+++ components/header.tsx
@@ -30,9 +30,9 @@ export default function Header() {
             >
               <Link
                 className={clsx(
-                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-100",
                   {
-                    "text-gray-950 dark:text-gray-200":
+                    "text-gray-950 dark:text-gray-100":
                       activeSection === link.name,
                   }
                 )}
