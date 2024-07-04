824cc5dfa05b28bc2ca5def3cd934d54c044c451
pokeuq <109028724+pokeuq@users.noreply.github.com>
2024-07-04 18:33:27 +0500

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,11 +4,12 @@ export default function Footer() {
   return (
     <footer className='mb-10 px-4 text-center text-gray-500'>
         <small className='mb-2 text-xs block'>
-            &copy; 2024 Bezhan. All rights reserved.
+            &copy; 2024 Bezhan. Все права защищены.
         </small>
         <p className='text-xs '>
-            <span className='font-semibold'>About this website:</span> build with React & Next.js (App Router & Server Actions), Tailwind CSS, Framer Motion, React Email & Resend, Vercel hosting.
+    <span className='font-semibold'>О сайте:</span> создан с использованием React & Next.js (App Router & Server Actions), Tailwind CSS, Framer Motion, React Email & Resend, хостинг на Vercel.
         </p>
+
     </footer>
   )
 }
